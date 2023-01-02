c06c094371a89a0723eabfe82be62c5e05157fcc
L9029 <angeloca03@gmail.com>
2023-01-02 15:52:52 -0400

    Se creo la eleccion del jugador con un input, se muestra la cantidad de los rounds cuando se repite el ciclo y las opeciones que tiene el jugador

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 import random
 
-options = ('piedra', 'papel', 'tijeras')
-
-computer_wins = 0
-user_wins = 0
-
-rounds = 1
-
 def main():
-    pass
+    options = (1, 2, 3)
 
-if __name__ == '_-main__':
+    computer_wins = 0
+    user_wins = 0
+    
+    rounds = 1
+    
+    while True:
+        
+        print('*' * 20)
+        print("""       ROUND {}""".format(rounds))
+        print('*' * 20)
+        
+        counters = """\nComputer wins: {} * User wins: {}\n""".format(computer_wins, user_wins)
+        print(counters)
+        
+        user_option = int(input("""1)Piedra \n\n2)Papel \n\n3)tijera \n\nElige: """))
+        
+        rounds += 1
+        try:
+            if not user_option in options:
+                print("La opcion no es valida, por favor intentelo nuevamente")
+                continue
+        except SyntaxError:
+            print("fumo")
+        break
+
+if __name__ == '__main__':
     main()
