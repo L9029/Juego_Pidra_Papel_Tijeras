d97eeaca363bde391b04fe061f69fa29455bff6f
L9029 <angeloca03@gmail.com>
2023-01-02 23:56:23 -0400

    Se agrego las opciones de la pc y también la selección de la pc, correción numerica de las opciones del jugador, prints de las opciones del jugador y la pc

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import random
 import time
 
 def main():
-    options = ('piedra', 'papel', 'tijera', "1", "2", "3")
+    options_selection = ('piedra', 'papel', 'tijera', "1", "2", "3")
+    computer_options = ('piedra', 'papel', 'tijera')
 
     computer_wins = 0
     user_wins = 0
@@ -11,22 +12,47 @@ def main():
     
     while True:
         
+        #Rounds actuales 
         print('*' * 20)
         print("""       ROUND {}""".format(rounds))
         print('*' * 20)
         
+        #Puntaje del jugador y de la computadora
         counters = """\nComputer wins: {} * User wins: {}\n""".format(computer_wins, user_wins)
         print(counters)
         
+        #Menu de opciones del usuario y input
         user_option = input("""1)Piedra \n\n2)Papel \n\n3)tijera \n\nElige: """)
         user_option = user_option.lower()
         
         rounds += 1
         
-        if not user_option in options:
-            print("La opcion no es valida, por favor intentelo nuevamente")
+        #Validacion de la eleccion del usuario
+        if not user_option in options_selection:
+            print("\nLa opcion no es valida, por favor intentelo nuevamente\n")
+            #Delay
             time.sleep(3)
             continue
+        
+        #Selección de la pc.
+        computer_option = random.choice(computer_options)
+        
+        #Corrección de opcion del jugador
+        if user_option == "1":
+            user_option = "piedra"
+            
+        elif user_option == "2":
+            user_option = "papel"
+            
+        elif user_option == "3":
+            user_option = "tijera"
+            
+        else:
+            pass
+        
+        #Opciones del jugador y la pc
+        print("\nOpción del jugador => ", user_option)
+        print("\nOpción del pc => ", computer_option)
         
         break
 
